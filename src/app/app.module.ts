24c16b5e082bbc4b1826ae103ecce5e273a6c775
Marcelo Santos <marcelegald2@outlook.com>
2023-02-18 11:02:52 -0300

    Atividade semana 07

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { CarouselComponent } from '../components/carousel/carousel.component';
 import { GaleryComponent } from '../components/galery/galery.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CarouselComponent,
     GaleryComponent,
   ],
   imports: [
